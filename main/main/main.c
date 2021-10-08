04037aa6ca8a4149fedb3522e38513a00314bf51
yuudai0501 <yuudai0501@icloud.com>
2021-10-08 19:07:52 +0900

    2021/10/08/19:07 基底変更後の最適基底数の調査後

--- main/main/main.c
+++ main/main/main.c
@@ -167,7 +167,7 @@ int main()
 	static char filename13[20] = { 't', 'e', 'x', 't', '.', 'b', 'm', 'p' };
 	static char filename14[20] = { 'e', 'a', 'r', 't', 'h', '.', 'b', 'm', 'p' };
 	static char filename15[20] = { 'm', 'a', 'n', 'd', 'r', 'i', 'l', 'l', '.', 'b', 'm', 'p' };
-	static char filename16[20] = { '1', '5', '.', 'b', 'm', 'p' };
+	static char filename16[20] = { '1', '0', '.', 'b', 'm', 'p' };
 
 	printf("\n******************\n 1, barbara\n 2, cameraman \n 3, mandrill \n 4, earth \n 5, Airplane \n 6, saiboat \n 7, boat \n 8, text \n 9, building \n ****************** \n\n filename plz .... : ");
 	scanf("%d", &i);
@@ -253,8 +253,10 @@ int main()
 	// ICAに"origin"を入れることで"y"(計算後の値)と"w"(計算の仕方)の結果が出力される
 	// 基底は計算方法。係数は 8*8の画素ブロックを構成するのに 64個の基底がそれぞれ どれくらい使われているのか（含まれているか）の値。
 	// ブロックとは 256*256画素のうち縦8横8のブロック。一画像につき(256/8) 32*32 = 1024ブロック
-	pcaStr = new_pca(origin);
-	ICA(origin, pcaStr, y, w, avg, 100, 0.002);
+	pcaStr = new_pca(origin_30);
+	ICA(origin_30, pcaStr, y, w, avg, 100, 0.002);
+
+	gnuplot(y);
 
 	static double** xxx;
 	xxx = (double**)malloc(sizeof(double*) * 64);
@@ -706,7 +708,7 @@ int main()
 
 		//fprintf(fp, "\n\n\n- - - - - - - - - - - - - - - - ( Reference ) For DCT - - - - - - - - - - - - - - - \n\n\n");
 		// 10段階品質があるから10段階分やる
-		for (Q = 40; Q > 0; Q -= 10) {
+		for (Q = 40; Q > 0; Q -= 100) {
 			printf("\r now Q is %d          \n", Q);
 
 
@@ -2105,8 +2107,18 @@ int main()
 					}
 				}
 
-
+				fprintf(fp9, "\n\n\n,Q%d\n,Area_num,,ica_basis2,",Q);
 				for (j = 0; j < 1024; j++) {
+					fprintf(fp9, "\n,%d,,%d,", j, (int)ica_basis2[64][j]);
+
+					for (i = 0; i < 5; i++) {
+						fprintf(fp9, ",%d", (int)full_mse[0][63-i][j]);
+					}
+					
+					if (ica_basis2[64][j] == 0) {
+						fprintf(fp9, ",,0_area");
+					}
+
 					no_op_1[j] = 0;
 					no_op_2[j] = 0;
 					no_op_3[j] = 0;
@@ -2139,17 +2151,20 @@ int main()
 							ny[c][j] = y[c][j];
 							no_op[j] = 1;
 							no_op_3[j] = 1;
+							fprintf(fp9, ",,%d,%d,%d", a, b, c);
 						}
 						else if (comb2[j][k][l][0] < comb[j][m][0] && dct_mse[j] > comb2[j][k][l][0] && comb2[j][k][l][1] > 0) {
 							ny[k][j] = y[k][j];
 							ny[l][j] = y[l][j];
 							no_op[j] = 1;
 							no_op_2[j] = 1;
+							fprintf(fp9, ",,%d,%d", k, l);
 						}
 						else if (dct_mse[j] > comb[j][m][0] && comb[j][m][1] > 0) {
 							ny[m][j] = y[m][j];
 							no_op[j] = 1;
 							no_op_1[j] = 1;
+							fprintf(fp9, ",,%d", m);
 						}
 					}
 					else if (a != 99 && b != 99 && c == 99) {//選出基底2津の時
@@ -2165,11 +2180,13 @@ int main()
 							ny[b][j] = y[b][j];
 							no_op[j] = 1;
 							no_op_2[j] = 1;
+							fprintf(fp9, ",,%d,%d", a, b);
 						}
 						else if (dct_mse[j] > comb[j][k][0] && comb[j][k][1] > 0) {
 							ny[k][j] = y[k][j];
 							no_op[j] = 1;
 							no_op_1[j] = 1;
+							fprintf(fp9, ",,%d", k);
 						}
 					}
 					else if (a != 99 && b == 99 && c == 99) {
@@ -2177,6 +2194,7 @@ int main()
 							ny[a][j] = y[a][j];
 							no_op[j] = 1;
 							no_op_1[j] = 1;
+							fprintf(fp9, ",,%d", a);
 						}
 					}
 
